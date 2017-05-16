9bfc0a1a5b224c6372c39e57f91e5e3f2805d44f
weizhen <939986850@qq.com>
2017-05-16 13:43:22 +0800

    第一次更新

--- app/src/main/java/com/bawei/redchild/MainActivity.java
+++ app/src/main/java/com/bawei/redchild/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //大脸盘送你一首歌“后来”
     }
 }
