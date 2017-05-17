868ddf7ebaffbd0587fdc8cb7f749d3e185723c5
bringeveryaimrise <bringeveryaimrise@outlook.com>
2017-05-17 19:04:30 +0800

    熊浩宇 版本1 主页面四个按钮

--- app/src/main/java/com/bawei/redchild/HomeActivity.java
+++ app/src/main/java/com/bawei/redchild/HomeActivity.java
@@ -1,11 +1,18 @@
 package com.bawei.redchild;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.bawei.redchild.base.BaseActivity;
 
-public class HomeActivity extends BaseActivity {
+public class HomeActivity extends BaseActivity{
+
+
 
 
     /**
@@ -22,6 +29,33 @@ public class HomeActivity extends BaseActivity {
      */
     @Override
     protected void initView() {
+        RadioGroup rg= (RadioGroup) findViewById(R.id.rg_home_show);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+
+                switch (checkedId){
+                    case R.id.rb_home_home_show:
+                        Toast.makeText(HomeActivity.this, "0", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rb_home_classify_show:
+                        Toast.makeText(HomeActivity.this, "1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rb_home_shoppingcart_show:
+                        Toast.makeText(HomeActivity.this, "2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rb_home_me_show:
+                        Toast.makeText(HomeActivity.this, "3", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rb_home_iv_show:
+                        Toast.makeText(HomeActivity.this, "4", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
 
     }
+
+
 }
