36b0a1ed6d4806c00b0e0d129b5fc490299fb1f5
bringeveryaimrise <bringeveryaimrise@outlook.com>
2017-05-17 14:43:43 +0800

    测试C分支操作

--- app/src/main/java/com/bawei/redchild/MainActivity.java
+++ app/src/main/java/com/bawei/redchild/MainActivity.java
@@ -9,10 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //熊浩宇你个山炮
-
-        //分支操作
-        //
 
     }
 
