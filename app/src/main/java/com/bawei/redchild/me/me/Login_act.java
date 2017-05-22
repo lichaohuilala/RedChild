a92eef7a5d93bcbdc884951a40a498be148df648
SmallYellowDuck <490411496@qq.com>
2017-05-22 22:00:55 +0800

    程千浪修改登录11

--- app/src/main/java/com/bawei/redchild/me/me/Login_act.java
+++ app/src/main/java/com/bawei/redchild/me/me/Login_act.java
@@ -42,7 +42,7 @@ public class Login_act extends AppCompatActivity implements View.OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_act);
         initView();
-        //程千浪11
+        //程千浪fff
     }
 
     private void initView() {
