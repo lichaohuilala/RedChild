00b02a8384b81efa44f8d502c38ad2a171d1f443
liuhongliangc <758215074@qq.com>
2017-05-24 16:59:02 +0800

    liuhongliang

--- app/src/main/java/com/bawei/redchild/groupon/URL.java
+++ app/src/main/java/com/bawei/redchild/groupon/URL.java
@@ -11,11 +11,11 @@ public class URL {
     public static final String ry="http://result.eolinker.com/y7fna4B18728b8965aa53caf37032864df9d353ca6f28ed?uri=lhl";
     public static final String my="http://result.eolinker.com/y7fna4B18728b8965aa53caf37032864df9d353ca6f28ed?uri=lhl2";
     public static final String ls="http://result.eolinker.com/y7fna4B18728b8965aa53caf37032864df9d353ca6f28ed?uri=lhl3";
-    public static final String znk="http://f.m.suning.com/api/pgs/cateView.do?version=2&cateId=4444&page=1&size=10";
-    public static final String sgsx="http://f.m.suning.com/api/pgs/cateView.do?version=2&cateId=1004&page=1&size=10";
-    public static final String mz="http://f.m.suning.com/api/pgs/cateView.do?version=2&cateId=1006&page=1&size=10";
-    public static final String ggmm="http://f.m.suning.com/api/pgs/cateView.do?version=2&cateId=1026&page=1&size=10";
-    public static final String send="http://f.m.suning.com/api/pgs/cateView.do?version=2&cateId=1008&page=1&size=10";
-    public static final String fzxb="http://f.m.suning.com/api/pgs/cateView.do?version=2&cateId=1007&page=1&size=10";
-    public static final String smdq="http://f.m.suning.com/api/pgs/cateView.do?version=2&cateId=1017&page=1&size=10";
+    public static final String znk="http://result.eolinker.com/y7fna4B18728b8965aa53caf37032864df9d353ca6f28ed?uri=lhl4";
+    public static final String sgsx="http://result.eolinker.com/y7fna4B18728b8965aa53caf37032864df9d353ca6f28ed?uri=lhl5";
+    public static final String mz="http://result.eolinker.com/y7fna4B18728b8965aa53caf37032864df9d353ca6f28ed?uri=lhl6";
+    public static final String ggmm="http://result.eolinker.com/y7fna4B18728b8965aa53caf37032864df9d353ca6f28ed?uri=lhl7";
+    public static final String send="http://result.eolinker.com/y7fna4B18728b8965aa53caf37032864df9d353ca6f28ed?uri=lhl8";
+    public static final String fzxb="http://result.eolinker.com/y7fna4B18728b8965aa53caf37032864df9d353ca6f28ed?uri=lhl9";
+    public static final String smdq="http://result.eolinker.com/y7fna4B18728b8965aa53caf37032864df9d353ca6f28ed?uri=lhl10";
 }
